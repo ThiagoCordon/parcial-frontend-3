02010705eccc79c94c10cc636c673707837bec1d
ThiagoCordon <thiagocordon8@gmail.com>
2023-08-16 21:56:15 -0300

    Terminando parcial

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,55 @@
+import { useState } from "react";
 import "./App.css";
 import Card from "./components/Card";
-
 function App() {
+  const [alumno, setAlumno] = useState({
+    nombre: "",
+    animal: "",
+  });
+
+  const [show, setShow] = useState(false);
+  const [error, setError] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (
+      alumno.nombre.length < 3 ||
+      alumno.nombre.startsWith(" ") ||
+      alumno.animal.length < 6
+    ) {
+      setShow(false);
+      setError(true);
+    } else {
+      setShow(true);
+      setError(false);
+    }
+  };
+
+  const handleChange = (event) =>
+    setAlumno({ ...alumno, nombre: event.target.value });
+
   return (
     <>
-      <h1>Carga de estudiantes</h1>
-      <form></form>
-      <Card />
+      <h1>Elige tu animal favorito</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Ingresa tu nombre"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          placeholder="Ingresa tu animal favorito"
+          onChange={(event) =>
+            setAlumno({ ...alumno, animal: event.target.value })
+          }
+        />
+        <button>ENVIAR</button>
+
+        {error && <h6>Por favor chequea que la información sea correcta</h6>}
+        {show ? <Card alumno={alumno} /> : null}
+      </form>
     </>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,10 @@
-const Card = () => {
-  return <div>Card</div>;
+const Card = ({ alumno }) => {
+  return (
+    <>
+      <h4>Hola {alumno.nombre}!</h4>
+      <h4>Sabemos que tu animal favorito es: {alumno.animal}</h4>
+    </>
+  );
 };
 
 export default Card;
